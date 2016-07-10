555d9acc0b25f09adc04bd36a2405e84c74b97f5
jbandura <jacek.bandura@netguru.pl>
2016-07-10 20:32:10 +0200

    add delete and put endpoints

--- app/controllers/api/v1/challenges.rb
+++ app/controllers/api/v1/challenges.rb
@@ -34,6 +34,38 @@ module API
           get do
             Challenge.find(params[:id])
           end
+
+          desc "Update specific challenge"
+          params do
+            requires :challenge, type: Hash do
+              requires :title, type: String
+              requires :content, type: String
+              requires :solution, type: String
+              requires :user_id, type: Integer
+              requires :category_id, type: Integer
+            end
+          end
+
+          put do
+            challenge_params = permitted_params[:challenge]
+            challenge = Challenge.find(params[:id])
+            authorize challenge, :update?
+            challenge.update!(
+              title: challenge_params[:title],
+              content: challenge_params[:content],
+              solution: challenge_params[:solution],
+              user_id: challenge_params[:user_id],
+              category_id: challenge_params[:category_id]
+            )
+          end
+
+          desc "Delete a challenge"
+          delete do
+            challenge = Challenge.find(params[:id])
+            authorize challenge, :destroy?
+            challenge.destroy
+            nil
+          end
         end
       end
     end
--- app/policies/challenge_policy.rb
+++ app/policies/challenge_policy.rb
@@ -1,4 +1,4 @@
-class CategoryPolicy < ApplicationPolicy
+class ChallengePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope
