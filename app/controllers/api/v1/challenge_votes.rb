a104ff3f236346152dca83ab8194f01a7c3eb28c
jbandura <jacek.bandura@netguru.pl>
2016-07-17 17:40:03 +0200

    implement adding and removing of votes

--- app/controllers/api/v1/challenge_votes.rb
+++ app/controllers/api/v1/challenge_votes.rb
@@ -4,7 +4,34 @@ module API
       include API::V1::Defaults
 
       resource :challenge_votes do
+        desc "Get all votes"
         get { ChallengeVote.all }
+
+        desc "Create a vote"
+        params do
+          requires :challenge_vote, type: Hash do
+            requires :challenge_id, type: Integer
+            requires :user_id, type: Integer
+          end
+        end
+        post do
+          uid = permitted_params[:challenge_vote][:user_id]
+          cid = permitted_params[:challenge_vote][:challenge_id]
+          if ChallengeVote.where(challenge_id: cid, user_id: uid).present?
+            return error!("This user has already voted for this challenge", 403)
+          else
+            ChallengeVote.create!(user_id: uid, challenge_id: cid)
+          end
+        end
+
+        route_param :id do
+          desc "Undo a vote"
+          delete do
+            vote = ChallengeVote.find(params[:id])
+            vote.destroy!
+            status 204
+          end
+        end
       end
     end
   end
--- spec/api/v1/challenge_votes_spec.rb
+++ spec/api/v1/challenge_votes_spec.rb
@@ -17,4 +17,37 @@ RSpec.describe "API::V1::ChallengeVotes" do
       expect(resp.length).to eq 3
     end
   end
+
+  describe "POST /api/v1/challenge_votes" do
+    let(:params) do
+      {
+        challenge_vote: {
+          challenge_id: challenge.id,
+          user_id: user.id
+        }
+      }
+    end
+
+    it "should record vote for a specific user" do
+      post api_url, params
+      expect(response.status).to eq(201)
+      last_vote = ChallengeVote.last
+      expect(last_vote.user.id).to eq(user.id)
+    end
+
+    it "shouldn't allow voting more than once" do
+      ChallengeVote.create(user_id: user.id, challenge_id: challenge.id)
+      post api_url, params
+      expect(response.status).to eq(403)
+    end
+  end
+
+  describe "DELETE /api/v1/challenge_votes/:id" do
+    let(:vote) { create(:challenge_vote, user_id: user.id, challenge_id: challenge.id) }
+    it "should undo vote" do
+      delete "#{api_url}/#{vote.id}"
+      expect(response.status).to eq(204)
+      expect(ChallengeVote.count).to eq(0)
+    end
+  end
 end
